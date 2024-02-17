25015d67bcc3f910b94b6da0f91638dd22c54697
Balu <balupatil4815@gmail.com>
2024-02-18 00:06:37 +0530

    App listening and Middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './env'});
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 
 dotenv.config({
@@ -10,3 +11,15 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8080, ()=>{
+        console.log(`Server running at port : ${process.env.PORT || 8080}`)
+    });
+    app.on("error", (error)=>{
+        console.log("ERR: ",error);
+        throw error;
+    })
+})
+.catch((error)=>{
+    console.log("MONGO db connection failed !!!", error);
+})
